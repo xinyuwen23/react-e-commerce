9dcb7cb3a2eebbd7c732177bcda206de1314e1c0
Xinyu Wen <me.xinyuwen@gmail.com>
2021-04-14 08:43:04 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 
+import store from './redux'
 import App from './app'
 
 const index = (
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 )
 
 ReactDOM.render(index, document.getElementById('root'))
---
+++ src/redux/index.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
+import reducer from './reducer'
+
+const composedEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancers = composedEnhancers(applyMiddleware(thunk))
+
+const store = createStore(reducer, enhancers)
+
+export default store
