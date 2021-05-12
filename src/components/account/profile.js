6e4b060bc433a2fa2618386907c1a112caf82442
Xinyu Wen <me.xinyuwen@gmail.com>
2021-05-12 16:38:01 -0700

    .

--- src/components/account/profile.js
+++ src/components/account/profile.js
@@ -1,16 +1,50 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Space } from 'antd'
+import { Space, Row, Col } from 'antd'
 
 class Profile extends React.Component {
   render() {
     const { user } = this.props
     return (
-      <Space style={{ padding: '30px 60px 30px 60px' }} direction='vertical' size='large'>
-        <h1>Profile</h1>
-        <div>Email: {user.email}</div>
-        <div>Name: {user.name}</div>
-      </Space>
+      <Row>
+        <Col span={5} />
+        <Col span={14}>
+          <Space
+            style={{ padding: '40px 100px 40px 100px', width: '100%' }}
+            direction='vertical'
+            size='large'
+          >
+            <h1>PROFILE</h1>
+            <Space
+              style={{
+                width: '100%',
+                padding: '50px',
+                border: '1px solid rgba(0, 0, 0, 0.1)',
+                borderRadius: '10px',
+                boxShadow: '0 0 2px rgba(0, 0, 0, 0.1)',
+                fontSize: 16,
+              }}
+              direction='vertical'
+              size='large'
+            >
+              <Row>
+                <Col style={{ textAlign: 'right', fontWeight: 'bold' }} span={6}>
+                  Email
+                </Col>
+                <Col span={1} />
+                <Col>{user.email}</Col>
+              </Row>
+              <Row>
+                <Col style={{ textAlign: 'right', fontWeight: 'bold' }} span={6}>
+                  Name
+                </Col>
+                <Col span={1} />
+                <Col>{user.name}</Col>
+              </Row>
+            </Space>
+          </Space>
+        </Col>
+      </Row>
     )
   }
 }
--- src/components/items/item.js
+++ src/components/items/item.js
@@ -33,12 +33,12 @@ class Item extends React.Component {
             <Col style={{ padding: '0px 70px 0px 90px' }} span={12}>
               {item.images && <Image width='100%' src={item.images[0]} />}
             </Col>
-            <Col style={{ padding: '40px 20px 0px 10px' }} span={12}>
+            <Col style={{ padding: '60px 20px 0px 10px' }} span={12}>
               <Space direction='vertical'>
                 <div style={{ fontSize: 24, fontWeight: 'bold' }}>{item.title}</div>
                 <Space direction='vertical' size='large'>
                   <div style={{ fontSize: 16 }}>Seller: {item.seller}</div>
-                  <div style={{ fontSize: 30, fontWeight: 'bold' }}>${item.price}</div>
+                  <div style={{ fontSize: 32, fontWeight: 'bold' }}>${item.price}</div>
                   <div>
                     <InputNumber
                       size='large'
@@ -59,7 +59,7 @@ class Item extends React.Component {
               </Space>
             </Col>
           </Row>
-          <Space direction='vertical' size='large'>
+          <Space style={{ padding: '0px 100px 0px 100px' }} direction='vertical' size='large'>
             <div style={{ fontSize: 24, fontWeight: 'bold' }}>About This Item</div>
             <ReactMarkdown>{item.description}</ReactMarkdown>
           </Space>
